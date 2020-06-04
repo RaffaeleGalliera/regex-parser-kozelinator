b93468ca5dfd9d1e965c2e81c2c51c8958a26873
Raffaele Galliera <gallieraraffaele@gmail.com>
2020-06-04 18:18:17 +0200

    feat: add term parser

--- Main.hs
+++ Main.hs
@@ -16,5 +16,38 @@ newtype Parser a = Parser
     { runParser :: String -> Maybe (String, a)
     }
 
+instance Functor Parser where
+    fmap f (Parser p) =
+        Parser $ \input -> do
+        (input', x) <- p input
+        Just (input', f x)
+    
+instance Applicative Parser where
+    pure x = Parser $ \input -> Just (input, x)
+    (Parser p1) <*> (Parser p2) =
+        Parser $ \input -> do
+        (input', f) <- p1 input
+        (input'', a) <- p2 input'
+        Just (input'', f a)
+
+instance Alternative Parser where
+    empty = Parser $ \_ -> Nothing
+    (Parser p1) <|> (Parser p2) =
+        Parser $ \input -> p1 input <|> p2 input
+
+regExp :: Parser RegExp
+regExp = undefined
+
+term :: Parser RegExp
+term = Term <$> foldr ((<|>) . charP) empty (['a'..'z'] ++ ['0'..'9'])
+
+charP :: Char -> Parser Char
+charP x = Parser f
+    where
+        f (y:ys)
+            | y == x = Just (ys, x)
+            | otherwise = Nothing
+        f[] = Nothing
+
 main :: IO ()
 main = undefined
